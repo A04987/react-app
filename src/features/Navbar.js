af2fe2a039b9b21367d4d696b61e8247eb0a02f2
A04987 <gasidit0anu@gmail.com>
2025-10-20 00:10:28 +0700

    sdsdg

--- src/features/Navbar.js
+++ src/features/Navbar.js
@@ -1,39 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom"; 
 
-function Navbar({className}) {
+function Navbar({ className }) {
   return (
     <header className={className}>
-      <a href="/" className="brand">
+      <Link to="/" className="brand">   
         Trendie
-      </a>
-      <a href="/create-product">Create product</a>
+      </Link>
+      <Link to="/create-product">       
+        Create product
+      </Link>
     </header>
   );
 }
 
 Navbar.propTypes = {
-  className: PropTypes.string.isRequired
+  className: PropTypes.string,
 };
 
 export default styled(Navbar)`
-   height: 48px;
-   width: 100%;
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   background-color: #ffffff;
-   border-bottom: 1px solid #dee2e6;
-   box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
+  height: 48px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #ffffff;
+  border-bottom: 1px solid #dee2e6;
+  box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
 
-   padding: 2rem;
-   position: fixed;
-   z-index: 100;
+  padding: 2rem;
+  position: fixed;
+  z-index: 100;
 
-   .brand {
-      font-weight: bold;
-      font-size: 1.5rem;
-    }
- `;
-
+  .brand {
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+`;
